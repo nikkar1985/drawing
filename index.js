717df9824509f90e1e8a86caf9ed4901cbcc4728
nikkar1985 <nkarountzos.ka@iekdelta360.edu.gr>
2026-03-05 18:08:03 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+const express=require('express');
+const app= express ;
+const http= require('http').Server(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT || 3000;
+app.use(express.static(_dirname+'/public'));
+
+function onConnection(socket) {
+socket.on('drawing',(data) => socket.bloadcast.emit('drawing',data))
+
+}
+
+io.on('connection', onConnection);
+http.listen(port, () => console.log('listening on port ' + port));
